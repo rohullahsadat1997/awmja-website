260a0f771c495af99852cdbc53e1288f039f1d77
Rohullah Sadat <rohullahsadat1997@gmail.com>
2026-09-08 09:55:26 +0330

    Configure production security settings

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 ALLOWED_HOSTS = [
     "awmja.org",
     "www.awmja.org",
