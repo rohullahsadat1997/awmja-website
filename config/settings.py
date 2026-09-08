895c88c3953a1d3cb866ee92c2d804aa1fd0ccbd
Rohullah Sadat <rohullahsadat1997@gmail.com>
2026-09-08 03:53:28 +0330

    Allow Railway host

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "awmja-website-production.up.railway.app",
+    "127.0.0.1",
+    "localhost",]
 
 
 # Application definition
