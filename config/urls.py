a49ad5e09a45f570ceb9110d68436c290d6e5a34
Rohullah Sadat <rohullahsadat1997@gmail.com>
2026-09-09 02:58:10 +0330

    Serve media files in production

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 from core.sitemaps import StaticPagesSitemap, ArticleSitemap
 
@@ -31,21 +31,23 @@ sitemaps = {
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
     path("i18n/", include("django.conf.urls.i18n")),
-
     path(
         "sitemap.xml",
         sitemap,
         {"sitemaps": sitemaps},
         name="django_sitemap",
     ),
-
     path("", include("core.urls")),
 ]
 
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
