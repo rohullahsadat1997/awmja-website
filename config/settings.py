60d72ce8688f781e103f2fc0e09903e23f71cf17
Rohullah Sadat <rohullahsadat1997@gmail.com>
2026-09-08 10:18:12 +0330

    Harden production HTTPS security

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,16 @@ CSRF_TRUSTED_ORIGINS = [
     "https://www.awmja.org",
     "https://awmja-website-production.up.railway.app",
 ]
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 # Application definition
 
